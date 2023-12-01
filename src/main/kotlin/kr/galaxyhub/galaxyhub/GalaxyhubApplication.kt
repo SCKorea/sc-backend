442b65827b55357679ac53d72f81ba0969c13c4e
seokjin8678 <seokjin8678@gmail.com>
2023-12-01 22:56:40 +0900

    build: 초기 프로젝트 설정

---
+++ src/main/kotlin/kr/galaxyhub/galaxyhub/GalaxyhubApplication.kt
@@ -0,0 +1,11 @@
+package kr.galaxyhub.galaxyhub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GalaxyhubApplication
+
+fun main(args: Array<String>) {
+	runApplication<GalaxyhubApplication>(*args)
+}
