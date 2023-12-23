40fb0ad755c69c7dbf1deb24aecfed1122a255c9
Seokjin Jeon <seokjin8678@gmail.com>
2023-12-23 20:42:00 +0900

    fix: "V2__add_member.sql" 누락된 컬럼 추가 (#48) (#49)

--- src/main/resources/db/migration/V2__add_member.sql
+++ src/main/resources/db/migration/V2__add_member.sql
@@ -1,6 +1,11 @@
 CREATE TABLE member
 (
-    id BINARY(16) NOT NULL,
+    id            BINARY(16)   NOT NULL,
+    social_id     VARCHAR(255) NOT NULL,
+    social_type   VARCHAR(255) NOT NULL,
+    nickname      VARCHAR(255) NOT NULL,
+    email         VARCHAR(255) NULL,
+    profile_image VARCHAR(255) NULL,
     CONSTRAINT pk_member PRIMARY KEY (id)
 );
 
