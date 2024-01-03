c622cb02d0f3d6edacc9976b5e9fc13eb4cb2c48
Laeng <Laeng@users.noreply.github.com>
2024-01-03 21:18:02 +0900

    feat: testcontainers 추가 (#16)

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,10 @@ val jsoupVersion = "1.17.1"
 val jjwtVersion = "0.12.3"
 val mockkVersion = "1.13.4"
 val springMockkVersion = "4.0.2"
+val testcontainersVersion = "1.19.3"
+val testcontainersMysqlVersion = "1.19.3"
 val kotestVersion = "5.7.2"
+val kotestExtensionTestcontainers = "2.0.2"
 val kotestExtensionSpring = "1.1.3"
 
 dependencies {
@@ -87,9 +90,14 @@ dependencies {
     // Spring Mockk
     testImplementation("com.ninja-squad:springmockk:$springMockkVersion")
 
+    // Testcontainers
+    testImplementation("org.testcontainers:testcontainers:$testcontainersVersion")
+    testImplementation("org.testcontainers:mysql:$testcontainersMysqlVersion")
+
     // Kotest
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:$kotestExtensionTestcontainers")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:$kotestExtensionSpring")
 
     // Mock Web Server
