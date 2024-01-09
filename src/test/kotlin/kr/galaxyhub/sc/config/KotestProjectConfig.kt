b54a7b63961aff685a07b0b84c4178790c68102b
seokjin8678 <seokjin8678@gmail.com>
2024-01-09 23:17:34 +0900

    test: 명시적으로 SpringExtension 사용하도록 변경

--- src/test/kotlin/kr/galaxyhub/sc/config/KotestProjectConfig.kt
+++
@@ -1,9 +0,0 @@
-package kr.galaxyhub.sc.config
-
-import io.kotest.core.config.AbstractProjectConfig
-import io.kotest.extensions.spring.SpringExtension
-
-class KotestProjectConfig : AbstractProjectConfig() {
-
-    override fun extensions() = listOf(SpringExtension)
-}
--- src/test/kotlin/kr/galaxyhub/sc/config/spec/IntegrationSpec.kt
+++ src/test/kotlin/kr/galaxyhub/sc/config/spec/IntegrationSpec.kt
@@ -1,9 +1,13 @@
 package kr.galaxyhub.sc.config.spec
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.extensions.spring.SpringExtension
 import kr.galaxyhub.sc.config.EnableTestcontianers
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 @EnableTestcontianers
-abstract class IntegrationSpec(body: DescribeSpec.() -> Unit = {}) : DescribeSpec(body)
+abstract class IntegrationSpec(body: DescribeSpec.() -> Unit = {}) : DescribeSpec(body) {
+
+    override fun extensions() = listOf(SpringExtension)
+}
